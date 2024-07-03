44420e29dae6b9695f883d8127d954951cdcf22a
Sheharyar Abid <102092254+sheharyarabid@users.noreply.github.com>
2024-07-03 14:34:08 +0500

    Changes to be committed: 	modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //DOM - Document object Model
+// console.log(window)
+
+// Single element selectors
+// console.log(document.getElementById('my-form'));  //No selector syntax required
+// console.log(document.querySelector('#my-form')); //add the relevant selector syntax id/class/element
+
+// Multiple element selectors
+// console.log(document.querySelectorAll('.item')); //Selects all elements
+// console.log(document.getElementsByClassName('.item')) //alternative is use classes for those elements
+console.log(document.getElementsByTagName('li'));
